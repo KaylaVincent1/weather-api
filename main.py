e14bce19da62746509257836564e66ca89ed2e25
Kayla <kaylavincent14@gmail.com>
2024-03-31 01:06:15 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# @app.route("/home")  # @-->referring
+# def home():
+#     return render_template("tutorial.html")  # render_templates needs to be in a specifically named folder --> templates
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
