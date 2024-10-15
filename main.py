8fa15db8b60b794a0e09d16dd2096b8ebd992e92
kayla <kaylavincent14@gmail.com>
2024-10-14 22:37:45 -0500

    Show station table on the homepage

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
+# from adodbapi.examples.xls_read import filename
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-
-# @app.route("/home")  # @-->referring
-# def home():
-#     return render_template("tutorial.html")  # render_templates needs to be in a specifically named folder --> templates
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data = stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    # zfill addes 0s to the string value
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['     TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5003)
